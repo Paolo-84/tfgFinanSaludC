4f72f1d84f148c46b828d63fabf41dc09e73e829
paolo <paoloIdrugo84@gmail.com>
2025-06-10 00:25:05 +0200

    Repo vaciado pero historial conservado

--- src/main/resources/static/js/header-script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const currentMonthElement = document.getElementById('current-month');
-    if (currentMonthElement) {
-        const now = new Date();
-        const options = { month: 'long', year: 'numeric' };
-        currentMonthElement.textContent = now.toLocaleDateString('es-ES', options).replace(/^\w/, c => c.toUpperCase());
-    }
-});
